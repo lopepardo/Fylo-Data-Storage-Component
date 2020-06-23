528803e89d73b0ad91d15e8976cc1e8f94201daa
LuisFelipeL <felipelop254@gmail.com>
2020-06-23 16:22:09 -0500

    initial

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import "../scss/main.scss";
+
+import "../img/bg-desktop.svg";
+import "../img/bg-mobile.svg";
+import "../img/icon-document.svg";
+import "../img/icon-folder.svg";
+import "../img/icon-upload.svg";
+import "../img/logo.svg";
